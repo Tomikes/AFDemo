4983a4bf71dd329ef2d688ed31a2fae427d3c531
Tomikes <mike_shang@outlook.com>
2016-08-01 08:31:27 +0800

    Initial Commit

---
+++ AFDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFDemo
+//
+//  Created by mike on 8/1/16.
+//  Copyright © 2016 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFDemo
+//
+//  Created by mike on 8/1/16.
+//  Copyright © 2016 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
